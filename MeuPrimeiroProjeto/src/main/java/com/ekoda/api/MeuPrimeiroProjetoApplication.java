9631da9dd1f5d5c18eda14c52e6b55dd8ba9febd
Erick Seigi Koda <ekoda@uolinc.com>
2020-11-10 11:29:07 -0300

    OK

--- MeuPrimeiroProjeto/src/main/java/com/ekoda/api/MeuPrimeiroProjetoApplication.java
+++ MeuPrimeiroProjeto/src/main/java/com/ekoda/api/MeuPrimeiroProjetoApplication.java
@@ -1,5 +1,6 @@
 package com.ekoda.api;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class MeuPrimeiroProjetoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+		
+
 	}
 
 }
